ce3ec953bbfb7c85dc73ffb70f9fde39d39691b5
Cristian Menguer <cristianmenguer@gmail.com>
2021-04-21 16:03:26 +0100

    add jest

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,7 @@
-test('sum two numbers', () => {
-    expect(1 + 2).toBe(3)
-    expect(4 + 2).toBe(6)
-    expect(7 + 6).toBe(13)
+import CreateAppointmentService from './CreateAppointmentService'
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', () => {
+        expect(3).toBe(3)
+    })
 })
